b597f12200766d6d6455a956cbaa2772bdf6c547
felixmaina <karani.maina2010@gmail.com>
2022-10-21 16:15:57 +0300

    add spring server and spring actuator dependencies

--- src/main/java/com/example/configproperties/ConfigPropertiesApplication.java
+++ src/main/java/com/example/configproperties/ConfigPropertiesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfigPropertiesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigPropertiesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigPropertiesApplication.class, args);
+    }
 
 }
